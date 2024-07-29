3ca6d4ed1ab92c26918df4816685b89bde446021
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2024-07-29 06:45:28 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("starting of a new chapter" + "chapter 1");
+        System.out.println("hi");
     }
 }
