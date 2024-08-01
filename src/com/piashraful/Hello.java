8e7a97a6b63d568b3908455799e829c7f3f55d99
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2024-08-01 07:01:14 +0600

    Done

--- src/com/piashraful/Hello.java
+++ src/com/piashraful/Hello.java
@@ -1,7 +1,35 @@
 package com.piashraful;
 
+import java.awt.*;
+
 public class Hello {
     public static void main(String[] vars) {
-        System.out.println("Hello heoo heou");
+        
+        int age = 30;
+        int myAge = 29;
+        int herAge = myAge;
+
+        String x = "helo";
+        String z = x;
+
+        System.out.println(x);
+        System.out.println(z);
+        System.out.println(age);
+        System.out.println(herAge);
+
+        Point point1 = new Point(1,2);
+        Point point2 = point1;
+        point1.x=5;
+        point1.y= 6;
+        System.out.println(point1);
+        System.out.println(point2);
+
+        String str1 = new String("Hello");
+        String str2 = str1;
+        str2.isEmpty();
+        System.out.println(str1);
+        System.out.println(str2.isEmpty());
+
+
     }
 }
